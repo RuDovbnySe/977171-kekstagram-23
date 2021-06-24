eec3bfec8c7ac2337c7579798101b8175a5c1515
Сергей Довбня <65117604+RuDovbnySe@users.noreply.github.com>
2021-06-24 23:34:07 +0300

    Отрисуй меня полностью (часть 2) - 5

--- js/big-photo.js
+++ js/big-photo.js
@@ -6,68 +6,73 @@ import {isEscEvent} from './util.js';
 const bigPicture = document.querySelector('.big-picture');
 const commentParent = bigPicture.querySelector('.big-picture__social');
 const closeBigPictureButton = bigPicture.querySelector('#picture-cancel');
+const socialComments = bigPicture.querySelector('.social__comments');
 
 // генерация списка с комментариями
+const cleanSocialComments = () => {
+  while(socialComments.hasChildNodes()) {
+    socialComments.removeChild(socialComments.lastChild);
+  }
+};
+
+cleanSocialComments();
+
 const createComments = similarComments;
 
 const similarCometFragment = document.createDocumentFragment();
 
-const createCommentContainer = () => {
-  const list = document.createElement('ul');
-  list.classList.add('social__comments');
+createComments.forEach(({avatar, name, message}) => {
+  const li = document.createElement('li');
+  li.classList.add('social__comment');
+  socialComments.appendChild(li);
+  const img = document.createElement('img');
+  img.classList.add('social__picture');
+  img.setAttribute('src', avatar);
+  img.setAttribute('alt', name);
+  img.setAttribute('width',35);
+  img.setAttribute('height', 35);
+  li.appendChild(img);
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('social__text');
+  paragraph.textContent = message;
+  li.appendChild(paragraph);
 
-  createComments.forEach(({avatar, name, message}) => {
-    const li = document.createElement('li');
-    li.classList.add('social__comment');
-    list.appendChild(li);
-    const img = document.createElement('img');
-    img.classList.add('social__picture');
-    img.setAttribute('src', avatar);
-    img.setAttribute('alt', name);
-    li.appendChild(img);
-    const paragraph = document.createElement('p');
-    paragraph.classList.add('social__text');
-    paragraph.textContent = message;
-    li.appendChild(paragraph);
-  });
-  similarCometFragment.appendChild(list);
+  socialComments.appendChild(li);
+});
+
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    // eslint-disable-next-line
+    closeBigPictureModal();
+  }
 };
-
-createCommentContainer();
 
 // показываем блок с полной картинкой, удалив класс hidden
 const openBigPictureModal = () => {
   bigPicture.classList.remove('hidden');
 
-  document.removeEventListener('keydown', (evt) => {
-    if (isEscEvent(evt)) {
-      evt.preventDefault();
-      bigPicture.classList.add('hidden');
-    }
-  });
+  document.addEventListener('keydown', onPopupEscKeydown);
 };
 
 const closeBigPictureModal = () => {
   bigPicture.classList.add('hidden');
 
-  document.removeEventListener('keydown', (evt) => {
-    if (isEscEvent(evt)) {
-      evt.preventDefault();
-      bigPicture.classList.add('hidden');
-    }
-  });
+  document.removeEventListener('keydown', onPopupEscKeydown);
 };
+
+const pictures = Array.from(photoOtherUsers.querySelectorAll('.picture'));
 
 photoOtherUsers.addEventListener('click', (evt) => {
   evt.preventDefault();
   // Индекс фото, которое кликнуто
-  const pictures = Array.from(photoOtherUsers.querySelectorAll('.picture'));
   const photoIndex = pictures.indexOf(evt.target.closest('.picture'));
 
   // загружаем атрибуты в разметку
   bigPicture.querySelector('.big-picture__img img').src = similarPhotos[photoIndex].url;
   bigPicture.querySelector('.likes-count').textContent = similarPhotos[photoIndex].likes;
-  bigPicture.querySelector('.comments-count').textContent = similarPhotos[photoIndex].comment;
+  bigPicture.querySelector('.comments-count').textContent = similarComments.length;
   bigPicture.querySelector('.social__caption').textContent = similarPhotos[photoIndex].description;
 
   commentParent.appendChild(similarCometFragment);
@@ -79,13 +84,6 @@ photoOtherUsers.addEventListener('click', (evt) => {
 //событие по нажатию 'Закрыть' мышкой для закрытия большого изображения
 closeBigPictureButton.addEventListener('click', (evt) => {
   closeBigPictureModal(evt);
-});
-
-//Событие по нажатию Escape для закрытия большого изображения
-document.addEventListener('keydown', (evt) => {
-  if (isEscEvent(evt)) {
-    closeBigPictureModal();
-  }
 });
 
 export {bigPicture};
